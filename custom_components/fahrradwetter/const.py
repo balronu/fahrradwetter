ddaf34016b01b25c7378c50b07a5e8586f6d516f
balronu <81955469+balronu@users.noreply.github.com>
2025-12-31 10:15:24 +0100

    Update const.py

--- custom_components/fahrradwetter/const.py
+++ custom_components/fahrradwetter/const.py
@@ -1,33 +1,21 @@
 DOMAIN = "fahrradwetter"
 
-CONF_SOURCE_MODE = "source_mode"  # "owm" oder "entities"
-
-# OWM Mode
-CONF_OWM_API_KEY = "owm_api_key"
+CONF_API_KEY = "api_key"
 CONF_LAT = "lat"
 CONF_LON = "lon"
-CONF_LANG = "lang"
-CONF_UNITS = "units"
-CONF_UPDATE_INTERVAL = "update_interval"
 
-# Entities Mode
-CONF_TEMP_ENTITY = "temp_entity"
-CONF_WIND_ENTITY = "wind_entity"
-CONF_RAIN_ENTITY = "rain_entity"
-CONF_FORECAST_ENTITY = "forecast_entity"
-CONF_FALLBACK_TEMP_ENTITY = "fallback_temp_entity"
+CONF_MODE = "mode"
+MODE_OWM_ONLY = "owm_only"
+MODE_LOCAL_ONLY = "local_only"
+MODE_HYBRID = "hybrid"
 
-# Common
-CONF_TIMES = "times"  # list like ["06:30","16:00"]
-CONF_MIN_TEMP = "min_temp"
-CONF_MAX_WIND_KMH = "max_wind_kmh"
-CONF_MAX_RAIN = "max_rain"
+CONF_LOCAL_TEMP_ENTITY = "local_temp_entity"
+CONF_LOCAL_WIND_ENTITY = "local_wind_entity"
+CONF_LOCAL_RAIN_ENTITY = "local_rain_entity"
 
-DEFAULT_TIMES = ["06:30", "16:00"]
-DEFAULT_MIN_TEMP = 10.0
-DEFAULT_MAX_WIND_KMH = 15.0
-DEFAULT_MAX_RAIN = 0.0
+CONF_LOCAL_WIND_UNIT = "local_wind_unit"
+WIND_UNIT_MS = "ms"
+WIND_UNIT_KMH = "kmh"
 
-DEFAULT_LANG = "de"
-DEFAULT_UNITS = "metric"
-DEFAULT_UPDATE_INTERVAL = 30  # minutes
+CONF_TIME_MORNING = "time_morning"   # "06:30"
+CONF_TIME_AFTERNOON = "time_afternoon"  # "16:00"
